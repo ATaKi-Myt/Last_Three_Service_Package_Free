49767e65a73763a71c2b687f18ad0619af971a1e
Last Three <1563119813@qq.com>
2025-04-23 17:49:03 +0800

    Update Update.sh

--- Services/Update.sh
+++ Services/Update.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# 定义颜色代码
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # 无颜色
 
-# 定义加速选项数组
 ACCELERATOR_OPTIONS=(
     "不加速（建议墙外用户使用）"
     "使用脚本自带加速链接: https://gitproxy.click/"
     "使用脚本自带加速链接: https://github.moeyy.xyz/"
     "使用脚本自带加速链接: https://github.tbedu.top/"
     "使用脚本自带加速链接: https://github.proxy.class3.fun/"
-    "使用脚本自带加速链接: https://ghfile.geekertao.top"
+    "使用脚本自带加速链接: https://ghfile.geekertao.top/"
     "使用脚本自带加速链接: https://github.proxy.class3.fun/"
     "使用脚本自带加速链接: https://github-proxy.lixxing.top/"
     "使用脚本自带加速链接: https://ghf.无名氏.top/"
@@ -21,16 +19,13 @@ ACCELERATOR_OPTIONS=(
     "手动输入加速链接（自行寻找加速链接）"
 )
 
-# 显示加速选项菜单
 echo "请选择加速选项:"
 for i in "${!ACCELERATOR_OPTIONS[@]}"; do
     echo "$((i + 1)). ${ACCELERATOR_OPTIONS[$i]}"
 done
 
-# 读取用户选择
 read -p "请输入选项编号: " choice
 
-# 根据用户选择设置加速链接
 case $choice in
     1)
         ACCELERATOR=""
@@ -74,10 +69,8 @@ case $choice in
         ;;
 esac
 
-# 定义下载链接
 url="${ACCELERATOR}https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Service_Package/refs/heads/main/Last_Three_Service_Package.sh"
 
-# 检测脚本是否存在并删除
 if [ -f "Last_Three_Service_Package.sh" ]; then
     rm "Last_Three_Service_Package.sh"
     echo -e "${GREEN}已删除原有的 Last_Three_Service_Package.sh 脚本。${NC}"
@@ -85,15 +78,12 @@ else
     echo -e "${GREEN}当前目录不存在 Last_Three_Service_Package.sh 脚本，将直接下载。${NC}"
 fi
 
-# 下载脚本
 wget "$url"
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}Last_Three_Service_Package.sh 脚本下载成功。${NC}"
     echo -e "${GREEN}更新完成，请重新运行 bash Last_Three_Service_Package.sh 指令。${NC}"
     sleep 5
-    # 获取父进程 ID
     PARENT_PID=$PPID
-    # 向父进程发送 SIGTERM 信号
     kill -TERM $PARENT_PID
     exit 0
 else
